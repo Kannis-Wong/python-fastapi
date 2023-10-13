b9cb951630b0c5575ac442d7c5c00909ae5d2ce8
kannis-wong <kanwong98829@gmail.com>
2023-10-14 02:01:36 +0800

    feat: UUID fixed

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from typing import List
 from fastapi import FastAPI
 from models import User, Gender, Role
-from uuid import uuid4
+from uuid import UUID
 
 
 app = FastAPI()
 
 db: List[User] = [
     User(
-        id=uuid4(),
+        id=UUID("f9e682b1-8ef3-422e-b208-921399cc9064"),
         first_name="Kan",
         last_name="Wong",
         gender=Gender.male,
         roles=[Role.admin]
     ),
     User(
-        id=uuid4(),
+        id=UUID("03ea79bb-21f8-41bd-bdda-cd9adb6eb68e"),
         first_name="Manda",
         last_name="Alpha",
         gender=Gender.female,
