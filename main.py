8e3730670f0b31fd5d4f68cbcfe6e846e72d14f8
kannis-wong <kanwong98829@gmail.com>
2023-10-13 23:54:53 +0800

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+from typing import List
+from fastapi import FastAPI
+from models import User, Gender, Role
+from uuid import uuid4
+
+
+app = FastAPI()
+
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="Kan",
+        last_name="Wong",
+        gender=Gender.male,
+        roles=[Role.admin]
+    ),
+    User(
+        id=uuid4(),
+        first_name="Manda",
+        last_name="Alpha",
+        gender=Gender.female,
+        roles=[Role.student, Role.user]
+    ),
+]
+
+
+@app.get("/")
+async def root():
+    return {"Hello": "Mundo"}
+
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
