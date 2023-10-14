2d44024dc98d5b16ddad14f193be70d6aadc2272
kannis-wong <kanwong98829@gmail.com>
2023-10-15 01:53:06 +0800

    feat: adding exception 404 for delete request

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing  import List
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models  import User, Gender, Role
 from uuid    import UUID
 import uvicorn
@@ -49,6 +49,10 @@ async def delete_user(user_id: UUID):
         if user.id == user_id:
             db.remove(user)
             return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exist!"
+    )
 
 
 if __name__ == "__main__":
